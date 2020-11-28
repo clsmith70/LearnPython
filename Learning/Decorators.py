20bb3808c8700758b792a7aa7899312a01ffda37
Chris Smith <ole088@gmail.com>
2020-11-28 14:53:30 -0500

    Added Decorators.py

---
+++ Learning/Decorators.py
@@ -0,0 +1,25 @@
+# Decorators.py
+# For some snazzy decorators, go to http://wiki.python.org/moin/PythonDecoratorLibrary
+def type_check(correct_type):
+    def check(old_function):
+        def new_function(arg):
+            if (isinstance(arg, correct_type)):
+                return old_function(arg)
+            else:
+                print("Bad Type")
+        return new_function
+    return check
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+print(times2(2))
+times2('Not A Number')
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+first_letter(['Not', 'A', 'String'])
