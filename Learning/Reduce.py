364fa8a0b425d51f8efb0553583d22a4f262c4c1
Chris Smith <ole088@gmail.com>
2020-11-28 22:30:21 -0500

    Map, Filter, Reduce code

--- Learning/Reduce.py
+++ Learning/Reduce.py
@@ -1,1 +1,22 @@
 # Reduce.py
+from functools import reduce
+
+numbers = [3, 4, 6, 9, 34, 12]
+
+def custom_sum(first, second):
+    return first + second
+
+result = reduce(custom_sum, numbers)
+print(result)
+print()
+
+## using initial
+from functools import reduce
+
+numbers = [3, 4, 6, 9, 34, 12]
+
+def custom_sum(first, second):
+    return first + second
+
+result = reduce(custom_sum, numbers, 10)
+print(result)
